69997885367b13112856ae023f36b338026be3c3
Junior Vanin <jvanin@thoughtworks.com>
2016-01-31 17:36:10 -0200

    Junior - Adds Log4J dependency in order to generate logs.

--- src/main/java/com.jvanin.stream/controller/HeathCheckController.java
+++ src/main/java/com.jvanin.stream/controller/HeathCheckController.java
@@ -1,14 +1,19 @@
 package com.jvanin.stream.controller;
 
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RestController
 public class HeathCheckController {
 
     @RequestMapping(value = "/healthcheck", method = RequestMethod.GET)
     public String get(){
+        Logger logger = Logger.getLogger(HeathCheckController.class);
+        logger.info("HealthCheck works properly.");
+
         return "OK";
     }
 }
