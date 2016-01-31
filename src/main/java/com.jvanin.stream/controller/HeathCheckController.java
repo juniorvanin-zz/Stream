0fe000cd4a332735d494f1b49732a1244003d766
Junior Vanin <jvanin@thoughtworks.com>
2016-01-31 17:41:10 -0200

    Junior - Adds lombok dependency which allows a clear code.

--- src/main/java/com.jvanin.stream/controller/HeathCheckController.java
+++ src/main/java/com.jvanin.stream/controller/HeathCheckController.java
@@ -1,18 +1,19 @@
 package com.jvanin.stream.controller;
 
-import org.apache.log4j.Logger;
+import lombok.extern.log4j.Log4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 
+@Log4j
 @RestController
 public class HeathCheckController {
 
     @RequestMapping(value = "/healthcheck", method = RequestMethod.GET)
     public String get(){
-        Logger logger = Logger.getLogger(HeathCheckController.class);
-        logger.info("HealthCheck works properly.");
+
+        log.info("HealthCheck works properly.");
 
         return "OK";
     }
