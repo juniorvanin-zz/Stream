eb8d9e56b17b5ba420ad5674b74fe2585d3e209d
Junior Vanin <jvanin@thoughtworks.com>
2016-02-01 13:34:35 -0200

    Junior - Changes Spring configuration from xml to class config.

---
+++ src/main/java/com/jvanin/stream/config/SpringConfig.java
@@ -0,0 +1,29 @@
+package com.jvanin.stream.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages={"com.jvanin.stream"})
+public class SpringConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
+    }
+
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        final InternalResourceViewResolver internalResolver = new InternalResourceViewResolver();
+        internalResolver.setPrefix("/WEB-INF/views/");
+        internalResolver.setSuffix(".jsp");
+        internalResolver.setContentType("text/html;charset=utf-8");
+        return internalResolver;
+    }
+
+}
--- src/main/java/com/jvanin/stream/controller/HeathCheckController.java
+++ src/main/java/com/jvanin/stream/controller/HeathCheckController.java
@@ -1,21 +1,22 @@
 package com.jvanin.stream.controller;
 
 import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 
 @Log4j
-@RestController
+@Controller
 public class HeathCheckController {
 
-    @RequestMapping(value = "/healthcheck", method = RequestMethod.GET)
-    public String get(){
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ModelAndView get(){
 
         log.info("HealthCheck works properly.");
 
-        return "OK";
+        return new ModelAndView("index");
     }
 }
 
