912d27fa8bff60ec9ca2e04e19e0b3344173f004
Junior Vanin <jvanin@thoughtworks.com>
2016-02-16 02:01:49 -0200

    Adds liquibase migrations support and rename application/spring config class.

--- src/main/java/com/jvanin/stream/config/ApplicationConfiguration.java
+++ src/main/java/com/jvanin/stream/config/ApplicationConfiguration.java
@@ -11,7 +11,7 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages={"com.jvanin.stream"})
-public class SpringConfig extends WebMvcConfigurerAdapter {
+public class ApplicationConfiguration extends WebMvcConfigurerAdapter {
     @Override
     public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
         configurer.enable();
